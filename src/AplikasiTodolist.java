8991f19c94645412fbf45e5972b10430bb064e33
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-12-04 14:59:29 +0700

    add model and business logic

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -1,5 +1,23 @@
 public class AplikasiTodolist {
+
+  public static String[] model = new String[10];
+
   public static void main(String[] args) {
+
+  }
+
+  // Menampilkan todo  list
+  public static void showTodoList(){
+
+  }
+
+  // Menambahkan todo ke list
+  public static void addTodoList() {
+
+  }
+
+  // Menghapus todo dari list
+  public static void removeTodoList() {
 
   }
 }
