37cede1d6cacba0dceab8512e5bb7365f2fa2e9e
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-12-04 15:14:07 +0700

    add logic method showTodoList()

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -11,8 +11,15 @@ public class AplikasiTodolist {
    */
 
   // Menampilkan todo list
-  public static void showTodoList(){
+  public static void showTodoList() {
+    for (var i = 0; i < model.length; i++) {
+      var todo = model[i];
+      var no = i + 1;
 
+      if (todo != null) {
+        System.out.println(no + ". " + todo);
+      }
+    }
   }
 
   // Menambahkan todo ke list
